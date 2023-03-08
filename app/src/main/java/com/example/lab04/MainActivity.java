2836d1e403f6d62f19d871edda6f46aa8d2bf969
tqchen17 <tqchen17@gmail.com>
2023-03-08 11:53:41 -0500

    03/08/23 lab04 finished up to layout 6

--- app/src/main/java/com/example/lab04/MainActivity.java
+++ app/src/main/java/com/example/lab04/MainActivity.java
@@ -9,6 +9,6 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.layout3);
+        setContentView(R.layout.layout6);
     }
 }
