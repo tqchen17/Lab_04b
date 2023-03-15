a918f46d6a3dea8956f5db7dbba6562d9b28bd26
tqchen17 <tqchen17@gmail.com>
2023-03-15 10:54:48 -0400

    03/15/23 - finished lab04 with automatic switch timer

--- app/src/main/java/com/example/lab04/MainActivity.java
+++ app/src/main/java/com/example/lab04/MainActivity.java
@@ -3,12 +3,48 @@ package com.example.lab04;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.MotionEvent;
+
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class MainActivity extends AppCompatActivity {
-
+int[] ids;
+int count = -1;
+Thread thread;
+Timer timer = new Timer();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.layout6);
+        setContentView(R.layout.layout1);
+        ids = new int[] {R.layout.layout1,R.layout.layout2,R.layout.layout3,
+                R.layout.layout4,R.layout.layout5,R.layout.layout6,R.layout.layout7};
+        thread = new Thread() {
+            @Override
+            public void run() {
+                try {
+                    while (!thread.isInterrupted()) {
+                        Thread.sleep(4000);
+                        runOnUiThread(new Runnable() {
+                            @Override
+                            public void run() {
+                                count = (++count)%ids.length;
+                                setContentView(ids[count]);
+                            }
+                        });
+                    }
+                } catch (InterruptedException e) {
+                }
+            }
+        };
+        thread.start();
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        if(event.getAction()==MotionEvent.ACTION_DOWN)
+            count = (++count)%ids.length;
+            setContentView(ids[count]);
+        return true;
     }
 }
